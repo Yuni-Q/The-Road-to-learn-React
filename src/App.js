7874f555dfe4257c95aa92ce36ba1a8e0e7dad73
Yuni-Q <carnotcycle2@naver.com>
2018-11-09 18:52:41 +0900

    Add

--- src/App.js
+++ src/App.js
@@ -21,10 +21,24 @@ const list = [
   },
 ];
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
   render() {
     return (
       <div className="App">
-        {list.map(item =>
+        {this.state.list.map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -32,11 +46,18 @@ class App extends Component {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                dismiss
+              </button>
+            </span>
           </div>
         )}
       </div>
     );
   }
 }
-
 export default App;
